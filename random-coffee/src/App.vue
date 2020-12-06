a3f6f725396499f1ab9c0dcd046d40b0f612f22c
Viktoriia Zakorchemna <viktoriia.zakorchemna@gmail.com>
2020-12-06 21:57:43 +0100

    feat: implement custom cursor

--- random-coffee/src/App.vue
+++ random-coffee/src/App.vue
@@ -1,58 +1,59 @@
 <template>
-  <div id="app">
-  <particles-bg type="custom" :config="config" :bg="true"/>
-  <Home/>
+  <div data-cursor-hover data-cursor-mix-blend-mode="difference" id="app">
+    <particles-bg type="custom" :config="config" :bg="true" />
+      
+    <cursor-fx data-cursor-hover
+        data-cursor-mix-blend-mode="difference" color-hover="#fff" color="#fff" />
+    <Home />
   </div>
 </template>
 
 <script>
-
-import Home from './components/home-page';
+import Home from "./components/home-page";
 import { ParticlesBg } from "particles-bg-vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-  Home,  ParticlesBg
+    Home,
+    ParticlesBg,
   },
-  data: function() {
+  data: function () {
     return {
       config: {
         num: [15, 15],
-      rps: 0.8,
-      radius: [4, 4],
-      life: [1.5, 1],
-      v: [3, 1],
-      tha: [-140, 140],
-      // body: "./img/icon.png", // Whether to render pictures
-      rotate: [20, 20],
-      alpha: [0.6, 5],
-      scale: [1.5, 0.1],
-      position: "all", // all or center or {x:1,y:1,width:100,height:100}
-      color: ["#8071DC", "#F2C94C"],
-      cross: "cross", // cross or bround
-      random: null,  // or null,
-      g: 1,    // gravity
-   
-      onParticleUpdate: (ctx, particle) => {
+        rps: 0.8,
+        radius: [4, 4],
+        life: [1.5, 1],
+        v: [3, 1],
+        tha: [-140, 140],
+        // body: "./img/icon.png", // Whether to render pictures
+        rotate: [20, 20],
+        alpha: [0.6, 5],
+        scale: [1.5, 0.1],
+        position: "all", // all or center or {x:1,y:1,width:100,height:100}
+        color: ["#8071DC", "#F2C94C"],
+        cross: "cross", // cross or bround
+        random: null, // or null,
+        g: 1, // gravity
+
+        onParticleUpdate: (ctx, particle) => {
           ctx.beginPath();
-         
+
           ctx.arc(particle.p.x, particle.p.y, particle.radius, 0, 2 * Math.PI, false);
           ctx.fillStyle = particle.color;
           ctx.lineWidth = 5;
           ctx.fill();
           ctx.closePath();
-      }
-
+        },
       },
-     
     };
-  }
-}
+  },
+};
 </script>
 
-<style lang="scss" >
-@import './styles/main.scss';
+<style lang="scss">
+@import "./styles/main.scss";
 #app {
   position: absolute;
   top: 0;
@@ -60,8 +61,7 @@ export default {
   right: 0;
   bottom: 0;
   z-index: 0;
-   background-color: $bgDark;
-   min-height: 100vh;
-
+  background-color: $bgDark;
+  min-height: 100vh;
 }
 </style>
--- random-coffee/src/main.js
+++ random-coffee/src/main.js
@@ -1,7 +1,14 @@
 import Vue from 'vue';
 import App from './App.vue';
 import VueParticlesBg from "particles-bg-vue";
+ // Plugin cursor
+ import CursorFx from '@luxdamore/vue-cursor-fx';
+ import '@luxdamore/vue-cursor-fx/dist/CursorFx.css';
 
+ // Install cursor
+ Vue.use(
+     CursorFx
+ );
 
 Vue.config.productionTip = false;
 Vue.use(VueParticlesBg);
