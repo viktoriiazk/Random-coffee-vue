8c1aa7935e86e53f0c14914f6ebd03a92ff0ae74
Viktoriia Zakorchemna <viktoriia.zakorchemna@gmail.com>
2020-12-07 18:18:46 +0100

    refactor: add hover effect on social links

--- random-coffee/src/components/social.vue
+++ random-coffee/src/components/social.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="social-wrap">
-    <a href="https://github.com/viktoriiazk" target="_blank"><img src="@/assets/social/github.svg" alt="Github logo"/></a>
-    <a href="https://www.linkedin.com/in/viktoriia-zakorchemna/" target="_blank"><img src="@/assets/social/linkedin.svg" alt="Linkedin logo" /></a>
-    <a href="https://www.facebook.com/viktoriia.zakorchemna/" target="_blank"><img src="@/assets/social/facebook.svg" alt="Facebook logo" /></a>
-    <a href="https://www.instagram.com/viktoriia_zakorchemna/" target="_blank"><img src="@/assets/social/instagram.svg" alt="Instagram logo" /></a>
+    <a href="https://github.com/viktoriiazk" target="_blank"><img src="@/assets/social/github.svg" alt="Github logo" class="social-logo"/></a>
+
+    <a href="https://www.linkedin.com/in/viktoriia-zakorchemna/" target="_blank"><img src="@/assets/social/linkedin.svg" alt="Linkedin logo" class="social-logo" /></a>
+    <a href="https://www.facebook.com/viktoriia.zakorchemna/" target="_blank"><img src="@/assets/social/facebook.svg" alt="Facebook logo" class="social-logo" /></a>
+    <a href="https://www.instagram.com/viktoriia_zakorchemna/" target="_blank"><img src="@/assets/social/instagram.svg" alt="Instagram logo" class="social-logo" /></a>
   </div>
 </template>
 <script>
-export default {};
+export default {
+ 
+};
 </script>
 
 <style lang="scss" scoped>
@@ -19,6 +22,9 @@ export default {};
   flex-direction: column;
   max-width: 150px;
   width: 100%;
- 
+ a {
+   @include hover-scale;
+   
+ }
 }
 </style>
